ba420e5ac3082c76638f90e0be0d7b1d020c9de1
Iheanacho Ekene Nicholas <93764522+DevNick21@users.noreply.github.com>
2022-11-06 23:48:50 +0100

    Initial Commit

---
+++ Credentails_Display.py
@@ -0,0 +1,11 @@
+#Code To display Name, Age and Department by IHEANACHO EKENE NICHOLAS 2017364031 ECE
+
+#Variable Declaration and Input request parsed into the required data types
+surname=str(input("Enter Your Surname: "))
+firstname=str(input("Enter Your Firstname: "))
+fullname=(surname+" "+firstname)
+age=int(input("How old are you: "))
+department=str(input("Enter your Department: "))
+
+#Result
+print("Your name is",fullname,",","Your age is",age,",","Your Department is",department,".")
---
+++ Simple_Calculator.py
@@ -0,0 +1,23 @@
+x = int(input("Input X: "))
+y = int(input("Input Y: "))
+
+print("What do you want ?")
+print("Type A for Additon")
+print("Type S for Subtraction")
+print("Type M for Multiplication")
+print("Type D for Division")
+print("Type AREA for Area of a Circle")
+
+arithmetic = input("Type now: ").upper()
+if arithmetic == "A":
+    print(x+y)
+if arithmetic == "S":
+    print(x-y)
+if arithmetic == "M":
+    print(x*y)
+if arithmetic == "D":
+    print(x/y)
+if arithmetic == "AREA":
+    z = float(input("Input the radius: "))
+    area = (3.14*z*z)
+    print (area)
